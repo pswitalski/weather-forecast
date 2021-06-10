187b1aa0f8f5dcd3fdefdc4c960b3e4c866991ea
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-10 13:51:32 +0200

    create api.js file

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 
+import api from './utils/api';
+
 export default {
   name: 'App',
   components: {
---
+++ src/utils/api.js
@@ -0,0 +1,7 @@
+const api = {
+    key: '44caec744f46cb6d314a5b4838f2bb8d',
+    main: 'api.openweathermap.org/data/2.5/weather'
+
+}
+
+export default api;
