ee77e27a454c86d8ea4b94228c82b4d83fdbda50
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-11 00:11:32 +0200

    working on weather data

--- src/views/Current.vue
+++ src/views/Current.vue
@@ -2,6 +2,15 @@
     <div class="current container">
         <LoadingSpinner v-if="isLoading" />
         <h2 v-if="!isLoading" >{{currentTarget.name}}, {{currentTarget.sys.country}}</h2>
+        <p v-if="!isLoading" >Temperature: {{currentTarget.main.temp.toFixed()}}&deg;C</p>
+        <p v-if="!isLoading" >Feels like: {{currentTarget.main.feels_like.toFixed()}}&deg;C</p>
+        <p v-if="!isLoading" >Today's maximum temperature: {{currentTarget.main.temp_max.toFixed()}}&deg;C</p>
+        <p v-if="!isLoading" >Today's mainimum temperature: {{currentTarget.main.temp_min.toFixed()}}&deg;C</p>
+        <p v-if="!isLoading" >Pressure: {{currentTarget.main.pressure}} hPa</p>
+        <p v-if="!isLoading" >Humidity: {{currentTarget.main.humidity}}%</p>
+        <p v-if="!isLoading" >Wind speed: {{currentTarget.wind.speed}} m/s</p>
+        <p v-if="!isLoading" >Weather: {{currentTarget.weather[0].description}}</p>
+        <p v-if="!isLoading" >icon</p>
     </div>
 </template>
 
