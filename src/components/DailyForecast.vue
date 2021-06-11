57224ce997cc3db062fe9b69ebdee4f4d550dc17
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-11 18:43:26 +0200

    create DailyForecast component

--- src/components/DailyForecast.vue
+++ src/components/DailyForecast.vue
@@ -1,17 +1,18 @@
 <template>
 <div class="container">
     <LoadingSpinner v-if="isLoading" />
-    hourly
+    daily
 </div>
 </template>
 
 <script>
 import LoadingSpinner from '../components/LoadingSpinner.vue';
+
 import axios from 'axios';
 import api from '../utils/api';
 
 export default {
-    name: 'HourlyForecast',
+    name: 'DailyForecast',
     components: {
         LoadingSpinner
     },
@@ -26,7 +27,7 @@ export default {
     },
     mounted() {
         this.isLoading = true;
-        axios.get(`${api.main}${api.oneCall}?lat=${this.coord.lat}&lon=${this.coord.lon}&exclude=minutely,daily,current${api.units}${api.key}`)
+        axios.get(`${api.main}${api.oneCall}?lat=${this.coord.lat}&lon=${this.coord.lon}&exclude=minutely,hourly,current${api.units}${api.key}`)
             .then(response => {
                 console.log(response.data);
                 this.OneCallResponse = response.data;
--- src/views/Forecast.vue
+++ src/views/Forecast.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="forecast">
         <HourlyForecast :coord="coord" />
+        <DailyForecast :coord="coord" />
     </div>
 </template>
 
 <script>
 
-
+import DailyForecast from '../components/DailyForecast.vue'
 import HourlyForecast from '../components/HourlyForecast.vue'
 
 export default {
     name: "Forecast",
     components: {
-        HourlyForecast
+        HourlyForecast,
+        DailyForecast
     },
     props: {
         coord: Object
