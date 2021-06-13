3b0787b36bcb3d66a38917dad278d01bd245a9d7
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-13 20:29:23 +0200

    working on charts

--- src/components/DailyForecast.vue
+++ src/components/DailyForecast.vue
@@ -1,12 +1,14 @@
 <template>
 <div class="container">
     <LoadingSpinner v-if="isLoading" />
+    <LineChart :chartData="chartData" :chartOptions="chartOptions" />
     daily
 </div>
 </template>
 
 <script>
 import LoadingSpinner from '../components/LoadingSpinner.vue';
+import LineChart from '../components/LineChart.vue';
 
 import axios from 'axios';
 import api from '../utils/api';
@@ -14,7 +16,8 @@ import api from '../utils/api';
 export default {
     name: 'DailyForecast',
     components: {
-        LoadingSpinner
+        LoadingSpinner,
+        LineChart
     },
     props: {
         coord: Object
@@ -22,15 +25,38 @@ export default {
     data() {
         return {
             isLoading: false,
-            OneCallResponse: {}
+            OneCallResponse: {},
+            chartData: {
+      labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      datasets: [
+        {
+          label: 'Temperature',
+          backgroundColor: '#ddd',
+          data: [10, 12, 9, 14, 10, 20, 18],
+          fill: false
+        },
+        {
+          label: 'Temperature',
+          backgroundColor: '#333',
+          data: [20, 12, 9, 14, 10, 20, 18],
+          fill: false
+        }
+        ],
+     },
+            chartOptions: {
+        responsive: true
+        }
         }
     },
     mounted() {
         this.isLoading = true;
         axios.get(`${api.main}${api.oneCall}?lat=${this.coord.lat}&lon=${this.coord.lon}&exclude=minutely,hourly,current${api.units}${api.key}`)
             .then(response => {
-                console.log(response.data);
+                console.log(response.data.daily);
                 this.OneCallResponse = response.data;
+
+
+
                 this.isLoading = false;
             })
             .catch(error => {
---
+++ src/components/LineChart.vue
@@ -0,0 +1,19 @@
+<script>
+import { defineComponent } from 'vue'
+import { Line } from 'vue3-chart-v2'
+
+export default defineComponent({
+  name: 'MonthlyChart',
+  extends: Line,
+  props: {
+      chartData: Object,
+      chartOptions: Object
+  },
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart(this.chartData,
+    this.chartOptions
+    )
+  }
+})
+</script>
