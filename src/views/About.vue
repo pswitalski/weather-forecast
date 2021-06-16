4f568afcc360a15605682c1ec2af5c299a86e915
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-16 17:00:15 +0200

    updated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container">
+    <h1>About</h1>
+    <section>
+      <p>All weather data comes from the Open Weather API.</p>
+    </section>
+    <section>
+      <p>Authors of free icons used on this site:</p>
+      <ul>
+        <li> Icon by <a href="https://freeicons.io/profile/3">icon king1</a> on <a href="https://freeicons.io">freeicons.io</a></li>
+      </ul>
+    </section>
+
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
