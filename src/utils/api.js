fc2b23960f2781dc19ba64bd5df23b6eba21e320
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-17 20:32:27 +0200

    change http to https in api requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const api = {
     key: '&appid=44caec744f46cb6d314a5b4838f2bb8d',
     units: '&units=metric',
-    main: 'http://api.openweathermap.org/',
+    main: 'https://api.openweathermap.org/',
     current: 'data/2.5/weather',
     air: '/data/2.5/air_pollution',
     reverse: 'geo/1.0/reverse',
